33f34f9788bd33f3085338c210627d682745cb77
sacha-r-p <sacha.ragris@insa-lyon.fr>
2024-05-27 10:21:30 +0200

    internship project commit

--- main script.py
+++ main script.py
@@ -18,17 +18,19 @@ with open('co2_intensity.csv', 'r') as CO2int_data:
 
 # Initialization of variables
 
-number_of_days = 7
+number_of_days = 1
+simulation_period = 1  # number of delivery periods in the simulation
 delivery_period = 24*number_of_days
 # delivery_mass = number_of_days*0.7*24*P_to_H2(1000, 33.3)
-delivery_mass = 2071
-initial_battery = 500
+delivery_mass = 150
+initial_battery = 0
 initial_hour = 4000
 
-PF_wind = PF_wind[initial_hour+0:initial_hour+delivery_period*2+48]
-PF_solar = PF_solar[initial_hour+0:initial_hour+delivery_period*2+48]
-price = price[initial_hour+0:initial_hour+delivery_period*2+48]
-CO2int = CO2int[initial_hour+0:initial_hour+delivery_period*2+48]
+
+PF_wind = PF_wind[initial_hour:]
+PF_solar = PF_solar[initial_hour:]
+price = price[initial_hour:]
+CO2int = CO2int[initial_hour:]
 
 # PF_wind = [random.uniform(0.4, 1) for _ in range(delivery_period+48)]
 # PF_solar = [random.uniform(0.4, 1) for _ in range(delivery_period+48)]
@@ -45,6 +47,7 @@ dr_pf_series = []
 dr_H2_prod = []
 
 i = 0
+j = 0
 time_left = delivery_period
 H2_mass_remaining = delivery_mass
 total_production = 0
@@ -53,38 +56,41 @@ total_electricity_cost = 0
 total_electricity_balance = (Annualized_cost(0, 50, 1000000, 10000)/8760)*delivery_period
 # Loop for the whole delivery period
 
-while i < number_of_days:
-    print(i)
-    ltp_PF_wind = PF_wind[10+i*24:delivery_period+34+i*24]
-    ltp_PF_solar = PF_solar[10+i*24:delivery_period+34+i*24]
-    ltp_price = price[10+i*24:delivery_period+34+i*24]
-    ltp_CO2int = CO2int[10+i*24:delivery_period+34+i*24]
+while j < simulation_period:
+    while i < number_of_days:
+        print(j, i)
+        ltp_PF_wind = PF_wind[10+i*24+j*delivery_period:delivery_period+34+i*24+j*delivery_period]
+        ltp_PF_solar = PF_solar[10+i*24+j*delivery_period:delivery_period+34+i*24+j*delivery_period]
+        ltp_price = price[10+i*24+j*delivery_period:delivery_period+34+i*24+j*delivery_period]
+        ltp_CO2int = CO2int[10+i*24+j*delivery_period:delivery_period+34+i*24+j*delivery_period]
 
-    hydrogen_plant = HydrogenProductionSystem(time_left, delivery_period, H2_mass_remaining, initial_battery, ltp_PF_wind, ltp_PF_solar, ltp_price, ltp_CO2int)
+        hydrogen_plant = HydrogenProductionSystem(time_left, delivery_period, H2_mass_remaining, initial_battery, ltp_PF_wind, ltp_PF_solar, ltp_price, ltp_CO2int)
 
-    hydrogen_plant.lt_planner()
-    hydrogen_plant.daily_planner()
-    hydrogen_plant.realisation()
+        hydrogen_plant.lt_planner()
+        hydrogen_plant.daily_planner()
+        hydrogen_plant.realisation()
 
-    ltp_pf_series = ltp_pf_series + hydrogen_plant.ltp_pf_series
-    ltp_H2_series = ltp_H2_series + hydrogen_plant.ltp_H2_series
-    ltp_H2_target = ltp_H2_target + hydrogen_plant.ltp_H2_target
-    dp_pf_series = dp_pf_series + hydrogen_plant.dp_pf_series
-    dp_Hydro_plan = dp_Hydro_plan + hydrogen_plant.dp_Hydro_plan
-    dr_pf_series = dr_pf_series + hydrogen_plant.dr_pf_series
-    dr_H2_prod = dr_H2_prod + hydrogen_plant.dr_H2_prod
+        ltp_pf_series = ltp_pf_series + hydrogen_plant.ltp_pf_series
+        ltp_H2_series = ltp_H2_series + hydrogen_plant.ltp_H2_series
+        ltp_H2_target = ltp_H2_target + hydrogen_plant.ltp_H2_target
+        dp_pf_series = dp_pf_series + hydrogen_plant.dp_pf_series
+        dp_Hydro_plan = dp_Hydro_plan + hydrogen_plant.dp_Hydro_plan
+        dr_pf_series = dr_pf_series + hydrogen_plant.dr_pf_series
+        dr_H2_prod = dr_H2_prod + hydrogen_plant.dr_H2_prod
 
-    total_emissions = total_emissions + hydrogen_plant.CO2_emissions
-    total_electricity_cost = total_electricity_cost + hydrogen_plant.electricity_cost
-    total_electricity_balance = total_electricity_balance + hydrogen_plant.electricity_balance
+        total_emissions = total_emissions + hydrogen_plant.CO2_emissions
+        total_electricity_cost = total_electricity_cost + hydrogen_plant.electricity_cost
+        total_electricity_balance = total_electricity_balance + hydrogen_plant.electricity_balance
 
-
-    i = i + 1
-    time_left = delivery_period - i*24
-    total_production = total_production + hydrogen_plant.total_production
-    H2_mass_remaining = delivery_mass - total_production
-    initial_battery = hydrogen_plant.battery_left
-
+        i = i + 1
+        time_left = delivery_period - i*24
+        total_production = total_production + hydrogen_plant.total_production
+        H2_mass_remaining = delivery_mass*(j+1) - total_production
+        initial_battery = hydrogen_plant.battery_left
+    i = 0
+    time_left = delivery_period
+    H2_mass_remaining = delivery_mass
+    j = j+1
 
 
 print(ltp_pf_series)
@@ -93,7 +99,7 @@ print(ltp_H2_target)
 print(dp_pf_series)
 print(dr_pf_series)
 print(total_production)
-print(delivery_mass)
+print(delivery_mass*j)
 # print(dr_pf_series[0][1].sum())
 print(total_electricity_balance/total_production)
 print(total_emissions/total_production)
