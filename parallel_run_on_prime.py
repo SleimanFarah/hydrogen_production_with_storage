1ca1e76009eb2278cdfa7bc2eb5eb97a169d72a7
Sleiman Farah <sleiman.farah@mpe.au.dk>
2024-07-31 10:03:52 +0200

    WIP: pushing to prime 2018, 2020, 2021, month, battery off

--- parallel_run_on_prime.py
+++ parallel_run_on_prime.py
@@ -16,7 +16,7 @@ def parallel_run(years, alphas, time_periods):
 if __name__ == "__main__":
 
     years = [2018, 2020, 2021]
-    time_periods = ["year"]
+    time_periods = ["month"]
 
     parser = argparse.ArgumentParser(description="parallel_processing_input")
     parser.add_argument('--alphas', type=float, nargs="+", help="alpha values")
